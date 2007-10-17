e95296576a0e3bbbf3e447ef70524b3117d87b63
stucki <stucki@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-10-17 09:20:06 +0000

    Takeover by the DAM development team
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/damactedittxt/trunk@7025 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,8 +14,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Media: Edit text files',
 	'description' => 'Add an action to the DAM for editing text files.',
 	'category' => 'be',
-	'author' => 'Rene Fritz',
-	'author_email' => 'r.fritz@colorcube.de',
+	'author' => 'The DAM development team',
+	'author_email' => 'typo3-project-dam@lists.netfielders.de',
 	'shy' => '',
 	'dependencies' => 'dam',
 	'conflicts' => '',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"0d00";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"5b8d";s:16:"locallang_db.xml";s:4:"7222";s:19:"doc/wizard_form.dat";s:4:"5ca5";s:20:"doc/wizard_form.html";s:4:"2dde";s:44:"modfunc1/class.tx_damactedittxt_modfunc1.php";s:4:"8b0a";s:22:"modfunc1/locallang.xml";s:4:"5b80";}',
+	'_md5_values_when_last_written' => '',
 );
 
 ?>
